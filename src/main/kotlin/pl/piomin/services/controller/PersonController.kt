67bad87a19cd7d78471c10d3700192e78bf69f77
Russell JVM Gutierrez <russell.gutierrez@zuhlke.com>
2020-11-16 14:38:58 +0800

    fix compilation issues

--- src/main/kotlin/pl/piomin/services/controller/PersonController.kt
+++ src/main/kotlin/pl/piomin/services/controller/PersonController.kt
@@ -3,18 +3,21 @@ package pl.piomin.services.controller
 import org.springframework.web.bind.annotation.*
 import pl.piomin.services.model.Person
 import pl.piomin.services.repository.PersonRepository
-
 @RestController
 @RequestMapping("/persons")
 class PersonController(var repository: PersonRepository) {
-    
-    public lateinit var mLetsviolate_many_rulesasPoSSSibble: Any?
-    private static val fooBar = "is a made up word"
+
+    public lateinit var mLetsviolate_many_rulesasPoSSSibble: Any
+
+    companion object {
+        private val fooBar = "is a made up word"
+    }
+
     private val barFoo = "all words are made up words"
-    
+
     fun notStatic(): String = fooBar
-    
-    private fun isItOrIsntIt(val flag: Boolean): String {
+
+    private fun isItOrIsntIt(flag: Boolean): String {
         with(flag) {
             if (!flag) {
                 if (barFoo == null) {
@@ -24,7 +27,7 @@ class PersonController(var repository: PersonRepository) {
         }
         return "String"
     }
-    
+
     protected fun seekMany(): List<Person> {
         return repository.findAll()
     }
@@ -45,5 +48,6 @@ class PersonController(var repository: PersonRepository) {
     fun remove(@PathVariable id: Int): Boolean = repository.removeById(id)
 
     private fun findEveryone() = repository.findAll()
-    
+
 }
+
