5c2fcefd6b5d2ce41b487a4376a9b9c1168e40cf
Russell JVM Gutierrez <russell.gutierrez@zuhlke.com>
2020-11-16 14:41:49 +0800

    more badness

--- src/main/kotlin/pl/piomin/services/Service.kt
+++ src/main/kotlin/pl/piomin/services/Service.kt
@@ -3,20 +3,21 @@ package pl.piomin.services.does.not.match.directory
 import pl.piomin.services.model.Person
 import pl.piomin.services.repository.PersonRepository
 
-public class Service {
-  static fun findAll() = repository.findAll()
+public class Service(val repository: PersonRepository) {
+  fun findAll() = repository.findAll()
   
-  private fun everyone(val useless: String) {
+  private fun everyone(useless: String): List<Person> {
     return repository.findAll()
   }
   
-  fun allOfThem(val flag: Boolean): Any {
-    if (flat == null) {
+  fun allOfThem(flag: Boolean): Any {
+    if (flag == null) {
       return true
     } else {
       return repository.findAll()
     }
   }
-  
-  
+
+
 }
+k
